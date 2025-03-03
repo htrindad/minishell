c384e6be2afff0f7ec44053306de191f9df2fdac
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-03-03 17:53:36 +0000

    neo_libft

---
+++ libs/neo_libft/ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 09:15:41 by htrindad          #+#    #+#             */
+/*   Updated: 2024/11/14 14:19:08 by htrindad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	unsigned char	ch;
+	size_t			i;
+
+	i = 0;
+	str = (unsigned char *)s;
+	ch = (unsigned char)c;
+	while (i < n)
+	{
+		if (str[i] == ch)
+			return (str + i);
+		i++;
+	}
+	return (NULL);
+}
