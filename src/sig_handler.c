80ec01f6cf0aa5de139434fcac8165bc0ca1764b
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-03-11 17:55:44 +0000

    SIGQUIT

--- src/sig_handler.c
+++ src/sig_handler.c
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 16:14:57 by htrindad          #+#    #+#             */
-/*   Updated: 2025/03/11 17:49:14 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/03/11 17:55:38 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	sig_handler(int sig, siginfo_t *s, void *content)
 {
-	// Theory
+	(void)content;
+	if (sig == SIGQUIT) // This needs to have more signs for Ctrl-C
+		exit(0);
 }
