ef4b982bbe151de4b1ca2e87b69a9db31a7fe645
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-03-11 17:49:20 +0000

    fc

---
+++ src/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 16:15:09 by htrindad          #+#    #+#             */
+/*   Updated: 2025/03/11 16:16:13 by htrindad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libs/neo_libft/libft.h"
+# include <signal.h>
+
+#endif
