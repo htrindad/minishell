b79c4fb00269d4d2a3a9f9ace0b3f449e127c4ab
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-04-16 19:30:34 +0100

    testing

--- src/lex/split_shell_ops.c
+++ src/lex/split_shell_ops.c
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 18:00:00 by htrindad          #+#    #+#             */
-/*   Updated: 2025/04/16 19:17:06 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/04/16 19:29:52 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,46 +41,46 @@ static size_t	count_cases(char const *s, t_ms *ms)
 	return (count);
 }
 
-static size_t	ft_count_words(char const *s, t_ms *ms, size_t max)
-{
-	size_t	i;
-	size_t	count;
-	char	*tmp;
-
-	i = 0; //len
-	count = 0; // space
-	while (s[i] && i < max)
-	{
-		while (s[i] == ' ')
-			i++;
-		if (!s[i])
-			break ;
-		count++;
-		tmp = ft_substr(s, i, 2);
-		if (is_special_char(s[i]) || s[i] == '\'' || s[i] == '\"')
-			i += iteration_cases(s, i, ms->scases, ms);
-		else
-		{
-			while (s[i] && s[i] != ' ' && !is_special_char(s[i]))
-				i += iterate_through_q(s, i, ms);
-		}
-		free(tmp);
-	}
-	return (count);
-}
-
-static char	**fill(char const *s, t_ms *ms)
-{
-	size_t	i;
-	size_t	count;
-	char	**ptr;
-
-	i = spec_case(s, ms->scases, NULL, 0);
-	count = ft_count_words(s, ms, i);
-	ptr = ft_calloc(count + 1, sizeof(char *));
-	if (ptr == NULL || sub(ptr, s, ms))
-		return (em("Error\nMalloc fail.\n", ms), NULL);
-}
+//static size_t	ft_count_words(char const *s, t_ms *ms, size_t max)
+//{
+//	size_t	i;
+//	size_t	count;
+//	char	*tmp;
+//
+//	i = 0; //len
+//	count = 0; // space
+//	while (s[i] && i < max)
+//	{
+//		while (s[i] == ' ')
+//			i++;
+//		if (!s[i])
+//			break ;
+//		count++;
+//		tmp = ft_substr(s, i, 2);
+//		if (is_special_char(s[i]) || s[i] == '\'' || s[i] == '\"')
+//			i += iteration_cases(s, i, ms->scases, ms);
+//		else
+//		{
+//			while (s[i] && s[i] != ' ' && !is_special_char(s[i]))
+//				i += iterate_through_q(s, i, ms);
+//		}
+//		free(tmp);
+//	}
+//	return (count);
+//}
+//
+//static char	**fill(char const *s, t_ms *ms)
+//{
+//	size_t	i;
+//	size_t	count;
+//	char	**ptr;
+//
+//	i = spec_case(s, ms->scases, NULL, 0);
+//	count = ft_count_words(s, ms, i);
+//	ptr = ft_calloc(count + 1, sizeof(char *));
+//	if (ptr == NULL || sub(ptr, s, ms))
+//		return (em("Error\nMalloc fail.\n", ms), NULL);
+//}
 
 size_t	op_funcs(char ***array, t_ms *ms)
 {
@@ -96,12 +96,12 @@ size_t	op_funcs(char ***array, t_ms *ms)
 	array = ft_calloc(count + 1, sizeof(char **));
 	if (array == NULL)
 		return (em("Error\nMalloc failed.\n", ms), 0);
-	while (ms->input[i])
-	{
-		i += iteration_cases(ms->input, i, ms->scases, ms);
-		array[arr++] = fill(ms->input + i, ms);
-		l = i;
-	}
+//	while (ms->input[i])
+//	{
+//		i += iteration_cases(ms->input, i, ms->scases, ms);
+//		array[arr++] = fill(ms->input + i, ms);
+//		l = i;
+//	}
 	array[count] = NULL;
 	return (count);
 }
