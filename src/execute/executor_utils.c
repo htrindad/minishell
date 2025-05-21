f594812fe631d6fb4fc186d52efc06e101006a19
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-05-21 19:44:32 +0100

    weird formatting

--- src/execute/executor_utils.c
+++ src/execute/executor_utils.c
@@ -6,7 +6,7 @@
 /*   By: mely-pan <mely-pan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 19:26:00 by mely-pan          #+#    #+#             */
-/*   Updated: 2025/05/21 19:42:26 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/05/21 19:44:25 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 bool	is_builtin(char *cmd)
 {
 	return (!ft_strncmp(cmd, "echo", 5) || !ft_strncmp(cmd, "cd", 3)
-		|| !ft_strncmp(cmd, "pwd", 4) || !ft_strncmp(cmd, "export", 7) || !ft_strncmp(cmd, "unset", 6) || !ft_strncmp(cmd, "env", 4)
+		|| !ft_strncmp(cmd, "pwd", 4) || !ft_strncmp(cmd, "export", 7) \
+		|| !ft_strncmp(cmd, "unset", 6) || !ft_strncmp(cmd, "env", 4) \
 		|| !ft_strncmp(cmd, "exit", 5));
 }
 
