1c297c87ec7924b84c83e6a032c61cd4430cec2d
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-08-16 21:26:35 +0100

    leaks and segs

--- src/builtins/dir.c
+++ src/builtins/dir.c
@@ -6,20 +6,42 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:22:09 by htrindad          #+#    #+#             */
-/*   Updated: 2025/06/16 19:55:39 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/08/16 21:26:16 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static inline t_env	*create_pwd(t_env *env)
+{
+	char	c[PATH_MAX];
+	char	*path;
+
+	path = ft_calloc(PATH_MAX + 5, 1);
+	if (path == NULL)
+		return (NULL);
+	getcwd(c, sizeof(c));
+	ft_strlcpy(path, "PWD=", 5);
+	ft_strlcat(path, c, 5 + PATH_MAX);
+	if (add_env(&env, path))
+		return (free(path), NULL);
+	free(path);
+	return (env);
+}
+
 t_env	*get_pwd(t_env *env)
 {
+	t_env	*first;
+
+	first = env;
 	while (env)
 	{
 		if (!ft_strncmp(env->key, "PWD", 3))
 			break ;
 		env = env->next;
 	}
+	if (env == NULL)
+		return (create_pwd(first));
 	return (env);
 }
 
@@ -55,6 +77,8 @@ void	set_pwd(char *c, size_t size, t_env *env, t_ms *ms)
 	t_env	*pwd;
 
 	pwd = get_pwd(env);
+	if (pwd == NULL)
+		em("Error\nMalloc Fail.\n", ms, 0);
 	ch_oldpwd(c, env, ms);
 	getcwd(c, size);
 	free(pwd->value);
