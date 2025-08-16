3c5a623d9500042fe5559c4478e1a014aa338990
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-08-16 21:45:04 +0100

    quotes

--- src/lex/split_shell_utils.c
+++ src/lex/split_shell_utils.c
@@ -6,7 +6,7 @@
 /*   By: mely-pan <mely-pan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 12:57:01 by mely-pan          #+#    #+#             */
-/*   Updated: 2025/08/11 17:40:01 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/08/16 21:44:48 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ size_t	iterate_through_q(const char *s, size_t i, t_ms *ms)
 		quote_type = s[i++];
 		while (s[i] && s[i] != quote_type)
 			i++;
-		if (!s[i])
+		if (!s[i] || s[i] == '\"' || s[i] == '\'')
 			return (em("Error\nUnclosed quotes", ms, 1), ft_strlen(s + start));
 		quote_type = 0;
 		while (s[i] && s[i] != ' ')
