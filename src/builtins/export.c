6d308f30163f939a31d3c5c8b65de6e1bc849f5c
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-08-19 19:00:11 +0100

    export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:12:35 by htrindad          #+#    #+#             */
-/*   Updated: 2025/08/17 19:31:20 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/08/19 18:59:01 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,10 +78,10 @@ int	bi_export(t_ms *ms)
 	arg = ms->tokens->value + 1;
 	if (!ms->tokens->value[1])
 		return (no_args(ms->env), 0);
-	if (replace(arg[0], ms->env))
-		return (1);
 	while (arg[i])
 	{
+		if (replace(arg[i], ms->env))
+			return (1);
 		if (check_exp_arg(arg[i]))
 		{
 			i++;
